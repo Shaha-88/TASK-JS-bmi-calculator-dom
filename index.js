a689bdc18d2692787219a051addf332c4bf1e654
Shaha-88 <shaha1988@hotmail.com>
2023-12-11 20:52:19 +0300

    function calaulatebmi

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  
+  let heightM = Height / 100;
+  let BMI = weight / (heightM * heightM);
+
+  alert(BMI);
+
+   if(BMI<18.5){alert("underweight");}
+   elseif(BMI>=18.5 && BMI(24.9)){alert("Normalweight");
+  }
+   elseif(BMI>=25 && BMI>=29.9){alert("overweight");
+  }
+    else {alert("Above obeisty")}
+
 }
